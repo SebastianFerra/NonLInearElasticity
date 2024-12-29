97e7ecc861a8d151f90c74b0549a73b988f3ff41
SebastianFerra <sferra@uc.cl>
2024-12-28 21:08:54 -0300

    nle not working and very very slow

--- problems.py
+++ problems.py
@@ -10,10 +10,10 @@ problem1 = [{"chi" :0.45, "phi0" : 0.3, "G": 0.15647831497059816,"dim":3},
             ,1,
             "3D_FreeSwell"]
 
-problem2 = [{"chi" :0.45, "phi0" : 0.3, "G": 0.15647831497059816,"dim":2},"geo_2D_bonded.pkl", {"dir_cond": "faces","DIR_FACES":"bottom"},2, "2D_bonded"]
+problem2 = [{"chi" :0.45, "phi0" : 0.3, "G": 0.13,"dim":2},"geo_2D_bonded.pkl", {"dir_cond": "faces","DIR_FACES":"bottom"},2, "2D_bonded"]
 
 
-problem3 = [{"chi" :0.45, "phi0" : 0.3, "G": 0.15647831497059816,"dim":3},"geo_3D_bonded.stp", {"dir_cond": "faces","DIR_FACES":"left"},2, "3D_bonded"]
+problem3 = [{"chi" :0.348, "phi0" : 0.2, "G": 0.13,"dim":3},"geo_3D_bonded.stp", {"dir_cond": "faces","DIR_FACES":"left"},2, "3D_bonded"]
 
 
 bonded = [2,3]
